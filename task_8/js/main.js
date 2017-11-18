2d09ebb2b8c6283e65e5f4bc898800e642706d92
Xsorter <xsorter@gmail.com>
2017-11-18 23:20:08 +0200

    task 8 - add credit functionality

--- task_8/js/main.js
+++ task_8/js/main.js
@@ -1,23 +1,27 @@
 'use strict'
 
-/*************************
- * Match pair by A.Nelin *
- *************************/
+/******************************
+ * Match pair game by A.Nelin *
+ ******************************/
 
 //running the game
 function gameRun(){
-
+    
     var data = {
-        score: 20,
+        score: 8,
         flippedArr: [],
         hiddenArr: [],
         render: renderCards(6), //Number of card pairs. Use even value. For now game have pictures only for 6 pairs max
         cardsDOM: document.querySelectorAll('.main__item-box'),
         dialog: document.querySelector('.dialog-wrapper'),
+        scoreDOM: document.querySelector('.score'),
         button: document.querySelector('#button'),
         buttonQuit: document.querySelector('#button-quit'),
+        dialogText: document.querySelector('.dialog-text'),
         //init method for new game
         newGame: function(){
+            this.score = 8;
+            this.scoreDOM.innerHTML = this.score;
             this.cardsDOM.forEach(function(e){
                 e.classList.remove('show');
                 e.classList.remove('cardhide');
@@ -26,8 +30,11 @@ function gameRun(){
             this.flippedArr = [];
             this.hiddenArr = [];
             this.dialog.classList.add('none');
+            this.dialogText.innerHTML = "";
         }
     }
+
+    data.scoreDOM.innerHTML = data.score;
 
     //generate array with a pair of classnames with numbers from 0 to 5
     function cardsGenerate (count){
@@ -54,7 +61,6 @@ function gameRun(){
     function renderCards(count){
         var shuffledArray = randomize(cardsGenerate(count));
         var wrapper = document.querySelector('.main__wrapper');
-
         for(var i = 0; i < shuffledArray.length; i++){
             wrapper.insertAdjacentHTML('beforeend', 
             '<div data-num="'+shuffledArray[i]+'" class="main__item-box flipped">\
@@ -83,6 +89,13 @@ function gameRun(){
     };
 
     function gameEnd(){
+        //first  - insert text to dialog window depend on win or loose status
+        if(data.score > 0){
+            data.dialogText.insertAdjacentHTML('beforeend', '<h2>Congratulations!</h2><p>You win the game!</p>');
+        }else{
+            data.dialogText.insertAdjacentHTML('beforeend', '<h2>Game over!</h2><p>You loose</p>');
+        }
+        //show dialog window
         data.dialog.classList.remove('none');
         data.button.addEventListener('click', function(){
             data.newGame()
@@ -95,6 +108,7 @@ function gameRun(){
     //iterate over array with card items
     data.cardsDOM.forEach(function(e){
 
+        //additionally add touchstart event for some mobile browsers
         ("click touchstart".split(" ")).forEach(function(el){
             e.addEventListener(el,cardsCheck,false);
         }); 
@@ -102,7 +116,6 @@ function gameRun(){
         function cardsCheck(){
             this.classList.remove('flipped');
             this.classList.add('show');
-
             if(this.classList.contains('show')){
                 data.flippedArr.push(this)
             }
@@ -112,8 +125,11 @@ function gameRun(){
                 cardHide();
             }else if(data.flippedArr.length >=2){
                 data.score--;
+                data.scoreDOM.innerHTML = data.score;
                 setTimeout(resetArray, 400);
-                console.log(data.score);
+                if(data.score <= 0){
+                    setTimeout(gameEnd(data.score), 500);
+                }
             };
     
             /*check if array with hidden elements 

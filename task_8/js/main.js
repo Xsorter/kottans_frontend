93a71925a119d05b9962485180b4884cd8691197
Xsorter <xsorter@gmail.com>
2017-11-18 16:09:05 +0200

    task 8 - add dialog for new game, code refactor

--- task_8/js/main.js
+++ task_8/js/main.js
@@ -1,18 +1,37 @@
 'use strict'
 
+/*************************
+ * Match pair by A.Nelin *
+ *************************/
+
 //running the game
-//TODO End game dialog
 function gameRun(){
-    
-    var flippedArr = [];
-    var hiddenArr = [];
-    var render = renderCards();
-    var cardsDOM = document.querySelectorAll('.main__item-box');
+
+    var data = {
+        flippedArr: [],
+        hiddenArr: [],
+        render: renderCards(6), //Number of card pairs. Use even value. For now game have pictures only for 6 pairs max
+        cardsDOM: document.querySelectorAll('.main__item-box'),
+        dialog: document.querySelector('.dialog-wrapper'),
+        button: document.querySelector('#button'),
+        buttonQuit: document.querySelector('#button-quit'),
+        //init method for new game
+        newGame: function(){
+            this.cardsDOM.forEach(function(e){
+                e.classList.remove('show');
+                e.classList.remove('cardhide');
+                e.classList.add('flipped');
+            });
+            this.flippedArr = [];
+            this.hiddenArr = [];
+            this.dialog.classList.add('none');
+        }
+    }
 
     //generate array with a pair of classnames with numbers from 0 to 5
-    function cardsGenerate (){
+    function cardsGenerate (count){
         var cards = [];
-        for(var i=0; i<6; i++){
+        for(var i=0; i<count; i++){
             cards.push('card__'+i,'card__'+i);
         }
         return cards
@@ -31,8 +50,8 @@ function gameRun(){
     }
 
     //insert divs with random classnames into DOM
-    function renderCards(){
-        var shuffledArray = randomize(cardsGenerate());
+    function renderCards(count){
+        var shuffledArray = randomize(cardsGenerate(count));
         var wrapper = document.querySelector('.main__wrapper');
 
         for(var i = 0; i < shuffledArray.length; i++){
@@ -47,46 +66,52 @@ function gameRun(){
     }
 
     function resetArray(){
-        cardsDOM.forEach(function(e){
+        data.cardsDOM.forEach(function(e){
             e.classList.remove('show');
             e.classList.add('flipped');
         });
-        flippedArr = [];
+        data.flippedArr = [];
     };
 
     function cardHide(){
-        flippedArr.forEach(function(e){
+        data.flippedArr.forEach(function(e){
             e.classList.add('cardhide');
-            hiddenArr.push(e);
-            flippedArr = [];
+            data.hiddenArr.push(e);
+            data.flippedArr = [];
         })
     };
 
     function gameEnd(){
-        alert('WIN!!')
-    }
+        data.dialog.classList.remove('none');
+        data.button.addEventListener('click', function(){
+            data.newGame()
+        });
+        data.buttonQuit.addEventListener('click', function(){
+            window.close()
+        });
+    };
     
     //iterate over array with card items
-    cardsDOM.forEach(function(e){
+    data.cardsDOM.forEach(function(e){
         e.addEventListener('click', function(){
             this.classList.remove('flipped');
             this.classList.add('show');
 
             if(this.classList.contains('show')){
-                flippedArr.push(this)
+                data.flippedArr.push(this)
             }
     
             /*hide pair of cards if they are identical*/
-            if(flippedArr.length >= 2 && flippedArr[0].dataset.num === flippedArr[1].dataset.num){
+            if(data.flippedArr.length >= 2 && data.flippedArr[0].dataset.num === data.flippedArr[1].dataset.num){
                 cardHide();
-            }else if(flippedArr.length >=2){
+            }else if(data.flippedArr.length >=2){
                 setTimeout(resetArray, 400)
             };
     
             /*check if array with hidden elements 
             already === array with all elements
             and fire game-end dialog*/ 
-            if(cardsDOM.length === hiddenArr.length){
+            if(data.cardsDOM.length === data.hiddenArr.length){
                 setTimeout(gameEnd, 1300)
             };
         });

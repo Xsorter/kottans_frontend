7f8e308b669d5dd874125204d1b833021d1dbaea
Xsorter <xsorter@gmail.com>
2018-02-05 23:05:11 +0200

    task 10 modules - architecture changed, webpack added

--- task_10-modules/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-import { data } from "./modules/config"
-import { find } from "./modules/test3"
-
-
-find('киев', data.key);
-
--- task_10-modules/assets/js/modules/config.js
+++
@@ -1,5 +0,0 @@
-let data = {
-    key: 'c6976a4c4e05421f9b4eaee7a311f0dc'
-}
-
-export { data }

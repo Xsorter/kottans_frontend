cb6ce3ee6f1a64ae60176d959fc4814ee0918c29
Xsorter <xsorter@gmail.com>
2018-02-13 00:48:40 +0200

    task 10 modules - made DOM optimisations in findCity function

--- task_10-modules/src/assets/js/render.js
+++ task_10-modules/src/assets/js/render.js
@@ -1,7 +1,6 @@
 import {parsedUrl, dataDOM, data} from "./config";
 import {pushHistory, showHistory} from "./localStorage";
 
-
 function addFavoriteButton(body) {
     dataDOM.titleDOM.insertAdjacentHTML(
       "beforeend",
@@ -25,36 +24,41 @@ function renderCity(body) {
   dataDOM.loaderDOM.classList.add("none");
   addFavoriteButton(body);
 
-  for (let i = 0; i < data.period; i++) {
-    dataDOM.mainDOM.insertAdjacentHTML(
-      "beforeend",
+  //create container for inserting data from loop
+  let documentFragment = document.createDocumentFragment();
 
-      //template with weather data
-      `<div class="content">
-          <div class="content__values">
-            <p>
-              <span class="caption__number">${body.data[i].temp}</span> ${data.unitsDisplay}
-              <p class="caption__title">avg. temp.</p> 
-            </p>
-            <object data="assets/media/${body.data[i].weather.icon}.svg" type="">
-            </object>
-            <p class="caption__title">${body.data[i].weather.description}</p> 
-          </div>
-          <p class="date">${body.data[i].datetime
-            .split("-")
-            .reverse()
-            .join(".")}
-          </p> 
-          <p>max. temp.: ${body.data[i].max_temp} ${data.unitsDisplay}</p>
-          <p>min. temp.: ${body.data[i].min_temp} ${data.unitsDisplay}</p>
-          <p>feels like, max: ${body.data[i].app_max_temp} ${data.unitsDisplay}</p>
-          <p>feels like, min: ${body.data[i].app_min_temp} ${data.unitsDisplay}</p>
-          <p>wind: ${body.data[i].wind_spd} m/s</p>
-          <p>precipitation: ${body.data[i].pop} %</p>
-      </div>
+  for (let i=0; i<data.period; i++) {
+    let contentWrapper = document.createElement("div");
+    contentWrapper.className = "content";
+    contentWrapper.insertAdjacentHTML("beforeend", 
+      `
+        <div class="content__values">
+          <p>
+            <span class="caption__number">${body.data[i].temp}</span> ${data.unitsDisplay}
+            <p class="caption__title">avg. temp.</p> 
+          </p>
+          <object data="assets/media/${body.data[i].weather.icon}.svg" type="">
+          </object>
+          <p class="caption__title">${body.data[i].weather.description}</p> 
+        </div>
+        <p class="date">${body.data[i].datetime
+          .split("-")
+          .reverse()
+          .join(".")}
+        </p> 
+        <p>max. temp.: ${body.data[i].max_temp} ${data.unitsDisplay}</p>
+        <p>min. temp.: ${body.data[i].min_temp} ${data.unitsDisplay}</p>
+        <p>feels like, max: ${body.data[i].app_max_temp} ${data.unitsDisplay}</p>
+        <p>feels like, min: ${body.data[i].app_min_temp} ${data.unitsDisplay}</p>
+        <p>wind: ${body.data[i].wind_spd} m/s</p>
+        <p>precipitation: ${body.data[i].pop} %</p>
       `
     );
+    documentFragment.appendChild(contentWrapper);
   }
+
+  dataDOM.mainDOM.appendChild(documentFragment);
+
 }
 
 export {renderCity};

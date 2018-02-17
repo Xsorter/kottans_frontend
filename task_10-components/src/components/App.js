3e162a5ce47414c8f83bc1c9781a809f0ace8af4
xsorter <xsorter@gmail.com>
2018-02-17 21:54:01 +0200

    task 10 components - changed App component

--- task_10-components/src/components/App.js
+++ task_10-components/src/components/App.js
@@ -3,16 +3,43 @@ import Render from "../assets/js/render";
 
 class App{
   constructor(){
-    this.locationElement = new LocationSearch();
-    this.mainElement = new Render();
+    this.state = {
+      city: new URLSearchParams(window.location.search).get('city') || ''
+    };
+    
+    this.locationElement = new LocationSearch({
+      city: this.state.city,
+      onSubmit: this.onSearchSubmit
+    });
 
-    this.rootElement = document.getElementById('root');
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    this.mainElement = new Render();
+    this.host = document.getElementById('root');
+  }
+
+
+  updateState(nextState){
+    this.state = Object.assign({}, this.state, nextState);
+    this.render();
+  }
+
+  onSearchSubmit(city){
+    this.updateState({ city });
   }
 
   render(){
-    this.rootElement.appendChild(this.locationElement.render());
-    this.rootElement.appendChild(this.mainElement.render());
+    const {city} = this.state;
+    this.host.innerHTML = '';
+
+    this.host.appendChild(this.locationElement.update({
+      city,
+      onSubmit: this.onSearchSubmit
+    }));
+
+    this.host.appendChild(this.mainElement.render());
+    return this.host
   }
+
 }
 
 export default App
--- task_10-components/src/components/LocationSearch.js
+++ task_10-components/src/components/LocationSearch.js
@@ -1,40 +1,54 @@
 import { findCity } from "./Search";
 
 class LocationSearch {
-    constructor() {
+    constructor(props) {
         this.state = {
             isValid : true
         }
+        this.props = props;
+
         this.handleSubmit = this.handleSubmit.bind(this);
+
         this.host = document.createElement('header');
         this.host.classList.add('location-wrapper');
         this.host.addEventListener('submit', this.handleSubmit);
     }
 
-    
+    update(nextProps){
+        this.props = nextProps;
+        return this.render();
+        console.log(this.props);
+    }
 
     updateState(nextState) {
-        this.state = Object.assign({}, this.state, nextState);
+        this.state = nextState;
         this.render();
     }
 
     handleSubmit(e){
         e.preventDefault();
         const city = e.target.elements.search.value.trim();
-        findCity(city);
+
+        findCity(this.props.city);
 
         if(!city.length){
             this.updateState({isValid: false})
+        }else{
+            this.props.onSubmit(city)
         }
     }
 
     render() {
         const {isValid} = this.state;
+        const {city} = this.props;
+        console.log(this.props);
+        
+
         this.host.innerHTML = `
             <h1 class="title">Weather-app</h1>
             <form class=${isValid ? '"weather-form"' : '"weather-form --invalid"'}>
                 <div class="search">
-                    <input name="search" required class="search-weather">
+                    <input name="search" required class="search-weather" value="${city}">
                     <button class="weather-search-submit">Search</button>.
                 </div>
             </form>

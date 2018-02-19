39fba5d82944d28a5ce41b3b7d81692d23a71b9b
Xsorter <xsorter@gmail.com>
2018-02-19 23:30:33 +0200

    task 10 components - Api component added

---
+++ task_10-components/src/components/Api.js
@@ -0,0 +1,15 @@
+import {parsedUrl, dataDOM, data} from "./config";
+
+const BASE_URL = 'https://api.weatherbit.io/v2.0/forecast';
+const getWeather = url => fetch(`${BASE_URL}${url}`)
+  .then(response => {
+    if (response.status !== 200) {
+      return Promise.reject(response)
+    }
+    return Promise.resolve(response)
+  })
+  .then(response => {
+    return response.json();
+  })
+export {getWeather};  
+

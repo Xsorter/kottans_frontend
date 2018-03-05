fab2b53e40c7c2c243081800479031dd0edfa3d6
Xsorter <xsorter@gmail.com>
2018-03-06 00:20:02 +0200

    router - add hashchange event

--- router/src/components/Router.js
+++ router/src/components/Router.js
@@ -1,14 +1,23 @@
 import {Component} from "../default/app"
 
 class Router extends Component {
-  constructor(host, routes) {
-    super(host, routes);
+  constructor(props) {
+    super(props);
     this.state = {
+      routes,
+      currentRoute: null,
+      currentComponent: null,
+    };
 
-    }
+    const {routes, host} = props;
 
     this.host = host;
-    window.addEventListener('hashChange', this.handleUrlChange);
+    this.handleUrlChange = this.handleUrlChange.bind(this);
+
+    window.addEventListener('hashchange', () =>
+      this.handleUrlChange(this.path)
+    );
+    
     console.log('test');
   }
 

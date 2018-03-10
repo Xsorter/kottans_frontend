5e8ff3d149cfd9bcffae82123bb663c5de133f71
xsorter <xsorter@gmail.com>
2018-03-10 20:12:24 +0200

    router: add createUser function

--- router/src/components/Register.js
+++ router/src/components/Register.js
@@ -10,8 +10,44 @@ class Register extends Component {
 
       this.host = document.createElement('div');
       this.getStores = this.getStores.bind(this);
+      this.createUser = this.createUser.bind(this);
     }
   
+
+    createUser(){
+      let data = {
+        "username": "someone",
+        "password": "qwerty12",
+        "password_repeat": "qwerty12",
+        "email": "some@gmail.com",
+        "store_id": 5,
+        "store_password": "qwerty11"
+      }
+
+      let header = new Headers();
+      header.append('Content-Type', 'application/json');
+      let method = 'POST';
+
+      fetch(`https://pizza-tele.ga/api/v1/user/create`, {
+        body: JSON.stringify(data),
+        method: method,
+        headers: header,
+        mode: 'cors'
+      })
+      .then(response => {
+        return response.json();
+      })
+      .then(function(body) {
+        if (body) {
+          console.log(body);
+          throw new Error(body.validations);
+        }
+      })
+      .catch(function(error){
+        console.log(error) 
+      })
+      
+    }
 
     getStores(){
       fetch(`https://pizza-tele.ga/api/v1/store/list`)
@@ -28,7 +64,7 @@ class Register extends Component {
             option.innerHTML = `${store.name}`;
             option.value = store.id;
             select.appendChild(option);
-            document.getElementById('form').appendChild(select);
+            document.getElementById('form').insertBefore(select, document.getElementById('form').childNodes[0]);
           })
           
         }
@@ -37,6 +73,8 @@ class Register extends Component {
 
     render() {
       this.getStores();
+      this.createUser();
+
     
       return `
         <div class="center-flex">

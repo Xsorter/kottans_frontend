01dfc1dbcce683b5c931910d6b84edf53c7a3d73
xsorter <xsorter@gmail.com>
2018-03-10 20:22:16 +0200

    router: add loginUser function

--- router/src/components/Login.js
+++ router/src/components/Login.js
@@ -5,9 +5,43 @@ class Login extends Component {
     constructor() {
       super();
       this.host = document.createElement('div');
+      this.loginUser = this.loginUser.bind(this);
+    }
+
+    loginUser(){
+      let data = {
+        "username": "someone",
+        "password": "qwerty12",
+      }
+
+      let header = new Headers();
+      header.append('Content-Type', 'application/json');
+      let method = 'POST';
+
+      fetch(`https://pizza-tele.ga/api/v1/user/login`, {
+        body: JSON.stringify(data),
+        method: method,
+        headers: header,
+        mode: 'cors'
+      })
+      .then(response => {
+        return response.json();
+      })
+      .then(function(body) {
+        if (body) {
+          console.log(body);
+          throw new Error(body.validations);
+        }
+      })
+      .catch(function(error){
+        console.log(error) 
+      })
+      
     }
   
     render() {
+      this.loginUser();
+
       return `
         <div class="center-flex">
           <div class="login">

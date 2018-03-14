4d5e657a8b433c4dbf838775f55b58c5067ff4e0
Xsorter <xsorter@gmail.com>
2018-03-14 23:57:52 +0200

    router - add fetch store list to register

--- router/src/components/Register.js
+++ router/src/components/Register.js
@@ -4,9 +4,25 @@ class Register extends Component {
     constructor(props) {
       super(props);
       this.host = document.createElement('div');
+      this.getStores = this.getStores.bind(this);
     }
   
+
+    getStores(){
+      const headers = new Headers();
+      headers.append('Content-Type' , 'application/x-www-form-urlencoded; charset=UTF-8');
+
+      fetch(`https://pizza-tele.ga/api/v1/store/list`)
+      .then(res => {
+        if (res.ok) return res.json();
+        console.log(res.json());
+        throw new Error(res.error);
+      });
+    }
+
     render() {
+      this.getStores();
+
       return `
         <div class="center-flex">
           <div class="register">

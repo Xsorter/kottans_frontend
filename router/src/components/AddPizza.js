d9beef0bea10a4856cd929ba21c6966caea637dd
xsorter <xsorter@gmail.com>
2018-03-24 22:20:46 +0200

    router - add form

--- router/src/components/AddPizza.js
+++ router/src/components/AddPizza.js
@@ -10,7 +10,35 @@ class AddPizza extends Component {
   
     render() {      
       return `
-        <div>2222</div>
+        <div class="add-container">
+
+            <h1>Add new pizza</h1>
+
+            <div class="add-wrapper">
+                <section>
+                    <h3>Preview</h3>
+
+                    <canvas width="320" height="320">
+                    </canvas>
+                </section>
+
+                <section>
+                    <h3>Pizza info</h3>
+
+                    <form>
+                        <input type="radio" checked name="pizza-size" id="size-small">
+                        <label for="size-small">Pizza 30 inch</label>
+
+                        <input type="radio" name="pizza-size" id="size-medium">
+                        <label for="size-medium">Pizza 45 inch</label>
+
+                        <input type="radio" name="pizza-size" id="size-big">
+                        <label for="size-big">Pizza 60 inch</label>
+                    </form>
+                </section>
+            </div>
+            
+        </div>
       `;
     }
   }

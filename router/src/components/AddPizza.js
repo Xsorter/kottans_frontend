0960e38150e0b461b76b1587116a4397c495b3c0
Xsorter <oleksii.nelin@innovecs.com>
2018-04-12 00:33:40 +0300

    router - addpizza form added

--- router/src/components/AddPizza.js
+++ router/src/components/AddPizza.js
@@ -4,6 +4,7 @@ import {Component} from "../default/app"
 class AddPizza extends Component {
     constructor() {
       super();
+
       this.state = {};
       this.host = document.createElement('div');
     }
@@ -26,17 +27,40 @@ class AddPizza extends Component {
                     <h3>Pizza info</h3>
 
                     <form>
-                        <input type="radio" checked name="pizza-size" id="size-small">
-                        <label for="size-small">Pizza 30 inch</label>
+                        <div>
+                            <label for="name">Pizza and order name</label>
+                            <input type="text" name="name" id="name" placeholder="Pizza and order name">
+                        </div>
 
-                        <input type="radio" name="pizza-size" id="size-medium">
-                        <label for="size-medium">Pizza 45 inch</label>
+                        <div>
+                            <h2>Pizza size</h2>
+                            <input type="radio" checked name="pizza-size" id="size-small">
+                            <label for="size-small">Pizza 30 inch</label>
 
-                        <input type="radio" name="pizza-size" id="size-big">
-                        <label for="size-big">Pizza 60 inch</label>
+                            <input type="radio" name="pizza-size" id="size-medium">
+                            <label for="size-medium">Pizza 45 inch</label>
 
-                        <button>Submit</button>
+                            <input type="radio" name="pizza-size" id="size-big">
+                            <label for="size-big">Pizza 60 inch</label>
+                        </div>
+
+                        <div>
+                            <h2>Description</h2>
+                            <textarea placeholder="Description">
+                            
+                            </textarea>
+                        </div>
+
+                        <div>
+                            <p>Total price:  $33.00</p>
+                        </div>
+
+                        <div>
+                            <button>Order</button>
+                            <button type="button">Cancel</button>
+                        </div>
                     </form>
+
                 </section>
             </div>
             
